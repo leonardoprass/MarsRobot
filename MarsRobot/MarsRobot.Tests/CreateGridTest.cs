f7c965342a4c6785c3e75908dcca3093a4ad3c74
Leonardo Prass <leonardo.prass@outlook.com>
2022-02-12 17:42:02 -0300

    Create grid tests - Red phase

--- MarsRobot/MarsRobot.Tests/CreateGridTest.cs
+++ MarsRobot/MarsRobot.Tests/CreateGridTest.cs
@@ -1,6 +1,24 @@
+using Xunit;
+
 namespace MarsRobot.Tests
 {
     public class CreateGridTest
     {
+        [Theory]
+        [InlineData(1,1)]
+        [InlineData(3,4)]
+        [InlineData(5,5)]
+        public void MatrixShouldHaveUserInputedSize(int rows, int columns)
+        {
+            int[,] matrix = CreateMatrix(rows, columns);
+
+            Assert.Equal(matrix.GetLength(0), rows);
+            Assert.Equal(matrix.GetLength(1), columns);
+        }
+
+        private int[,] CreateMatrix(int rows, int columns)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
