1537c65df483c1e21aa3e61c29173f3545adf7bf
Leonardo Prass <leonardo.prass@outlook.com>
2022-02-12 17:51:12 -0300

    Create grid tests - Refactor phase

---
+++ MarsRobot/MarsRobot.Application/Grid/Grid.cs
@@ -0,0 +1,13 @@
+﻿namespace MarsRobot.Application.Grid
+{
+    public static class Grid
+    {
+        public static int[,] CreateMatrix(int rows, int columns)
+        {
+            if (rows <= 0 || columns <= 0)
+                throw new ArgumentException("Invalid plateau size.");
+
+            return new int[rows, columns];
+        }
+    }
+}
--- MarsRobot/MarsRobot.Tests/CreateGridTest.cs
+++ MarsRobot/MarsRobot.Tests/CreateGridTest.cs
@@ -1,3 +1,4 @@
+using MarsRobot.Application.Grid;
 using System;
 using Xunit;
 
@@ -11,7 +12,7 @@ namespace MarsRobot.Tests
         [InlineData(5, 5)]
         public void MatrixShouldHaveUserInputedSize(int rows, int columns)
         {
-            int[,] matrix = CreateMatrix(rows, columns);
+            int[,] matrix = Grid.CreateMatrix(rows, columns);
 
             Assert.Equal(matrix.GetLength(0), rows);
             Assert.Equal(matrix.GetLength(1), columns);
@@ -20,17 +21,9 @@ namespace MarsRobot.Tests
         [Fact]
         public void MatrixShouldBeAtleast1x1()
         {
-            Action act = () => CreateMatrix(0, 1);
+            Action act = () => Grid.CreateMatrix(0, 1);
 
             Assert.Throws<ArgumentException>(act);
-        }
-
-        private int[,] CreateMatrix(int rows, int columns)
-        {
-            if (rows <= 0 || columns <= 0)
-                throw new ArgumentException("Invalid plateau size.");
-
-            return new int[rows, columns];
         }
     }
 }
